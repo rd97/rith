2762f033133cab13b36556c11011bbeb78bff65d
rd97 <31499424+rd97@users.noreply.github.com>
2018-01-21 06:31:29 +0000

    Create 23.cpp

---
+++ 23.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,a[50],min,i;
+	cin>>n;
+            cin>>a[1];
+            for(i=2;i<=n;i++)
+	{
+		{
+		cin>>a[i];
+	           
+	}
+		if(min>=a[i])
+		{
+			min=a[i];
+		}
+}
+                cout<<min;
+	
+	return 0;
+}
