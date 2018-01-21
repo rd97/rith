344b362e4f2f82fc1ae37c6055bce6ebd6255308
rd97 <31499424+rd97@users.noreply.github.com>
2018-01-21 04:30:38 +0000

    Create 43.cpp

---
+++ 43.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+char a[100]="its me rd";
+char s[100]="karur";
+int i,j;
+for(i=0;a[i]!='\0';i++)
+{
+	for(j=0;s[j]!='\0';j++)
+	{
+	a[i]=s[j];
+    }/*if(w!=0)
+    {
+    	a[100]=s[100];
+    }*/
+    s[i]='\0';
+	
+
+    cout<<a[100];
+}
+ return 0;
+    }
