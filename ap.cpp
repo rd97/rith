585832295c4a73c54ff37d203605d76e020b6ebf
rd97 <31499424+rd97@users.noreply.github.com>
2018-01-21 05:27:20 +0000

    Create ap.cpp

---
+++ ap.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=3,a=1,d=1,sum=0,i,y=0;
+	for(i=a;i<=n;i=i+d)
+	{
+	/*	if(y==0)
+		{ 
+			a=sum;
+			y=1;
+		}
+		
+else*/
+	
+	sum=sum+i;} 
+	{
+	cout<<sum;
+		
+	}
+return 0;
+}
