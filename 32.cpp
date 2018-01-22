d029332994697696ed6ac6b85741356421f91d9b
rd97 <31499424+rd97@users.noreply.github.com>
2018-01-23 03:40:05 +1300

    Create 32.cpp

---
+++ 32.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{ char c[20]="its me rd";
+int i,s=1;
+for(i=0;c[i]!=0;i++)
+{
+	if(c[i]==' ')
+	s++;
+}
+ cout<<s;
+ return 0;  
+}
