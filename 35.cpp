7ba8d97eb397c793901d79edf35a3dc15b107ef3
rd97 <31499424+rd97@users.noreply.github.com>
2018-01-23 05:16:58 +1300

    Create 35.cpp

---
+++ 35.cpp
@@ -0,0 +1,15 @@
+#include<string.h>
+using namespace std;
+
+int main()
+{ char c[30]="its me rd.12344567";
+int i,s=0;
+for(i=0;c[i]!=0;i++)
+{
+	if((c[i]>='1')&&(c[i]<='9'))
+	s++;
+
+}
+ cout<<s;
+ return 0;
+}
