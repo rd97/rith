8a3b01d94227bf861ee5059c6970e64e8a73008c
rd97 <31499424+rd97@users.noreply.github.com>
2018-01-23 05:39:14 +1300

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,temp;
+	a=37;
+	b=73;
+   
+     temp=a;
+     a=b;
+    b=temp;
+     cout<<a;
+     cout<<b;
+     
+	return 0;
+}
