5521bb3ba0028f67b06fc29973bce59c7f9f6aee
rd97 <31499424+rd97@users.noreply.github.com>
2018-01-24 04:22:27 +1300

    Update 25.cpp

--- 25.cpp
+++ 25.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-	int a[200],i,n=3,k,temp;
+	int a[200],i,n=4,k,temp;
 	for(i=0;i<=n;i++)
 	{
 		cin>>a[i];
 	}
-	for(i=0;i<n;i++)
+	for(i=0;i<=n;i++)
 	{
-	for(k=0;k<(n-i-1);k++)
+	for(k=0;k<=(n-i-1);k++)
 	{ 
 		if(a[i]>a[k+1])
 		{
@@ -23,3 +23,4 @@ int main()
 	cout<<a[n/2]<<'\t';}
 	return 0;
 }
+  
