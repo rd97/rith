22991368c9d2ee44f9999d608a4b0ea545be40a9
rd97 <31499424+rd97@users.noreply.github.com>
2018-01-24 04:13:12 +1300

    Create 25.cpp

---
+++ 25.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[200],i,n=3,k,temp;
+	for(i=0;i<=n;i++)
+	{
+		cin>>a[i];
+	}
+	for(i=0;i<n;i++)
+	{
+	for(k=0;k<(n-i-1);k++)
+	{ 
+		if(a[i]>a[k+1])
+		{
+	    temp=a[i];
+	    a[i]=a[k+1];
+	   a[k+1]= temp;
+	}
+	}
+	}
+	{
+	cout<<a[n/2]<<'\t';}
+	return 0;
+}
