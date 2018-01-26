d8d6f926f0d93061c9be84c7db2f245ad0d7df62
rd97 <31499424+rd97@users.noreply.github.com>
2018-01-26 08:41:27 +0530

    Create 55.cpp

---
+++ 55.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n[50],m[50],p=0,i,c;
+ for(i=0;i<=p;i++)
+ {
+ 	cin>>n[i];
+ 	cin>>m[i];
+ }
+ c=n[i]*m[i];
+ if(c%2==0)
+ {
+ 	cout<<"even";
+ }else
+ {
+ 	cout<<"odd";
+ }
+
+      return 0;
+}
