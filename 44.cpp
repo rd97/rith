801a1f9985fe131e0ef8d68e034ccf261ec96d00
rd97 <31499424+rd97@users.noreply.github.com>
2018-01-29 19:22:24 +0530

    Create 44.cpp

---
+++ 44.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,i;
+{
+cin>>a;
+}
+	if((a>=0)&&(a<=10))
+	{
+		cout<<"yes";
+	}
+		else
+		{
+		cout<<"no";
+		}
+	
+	
+	return 0;
+	
+}
