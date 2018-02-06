b2aa7623ee880c5d98f935010325cae621f3a01b
rd97 <31499424+rd97@users.noreply.github.com>
2018-02-06 21:41:27 +0530

    Create 61.cpp

---
+++ 61.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a[40];
+    int i,k;
+    cout<<"string";
+    cin>>a;
+    cout<<"number";
+    cin>>k;
+    for(i=0;i<k;i++)
+    {
+        cout<<a[i];
+    }
+    return 0;
+}
