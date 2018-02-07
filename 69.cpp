a216e9f1a2c68137aa30f5132d0e026cf95d231f
rd97 <31499424+rd97@users.noreply.github.com>
2018-02-07 15:41:42 +0530

    Create 69.cpp

---
+++ 69.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m,s=0;
+    cin>>n>>m;
+    s=n-m;
+    if(s%2==0)
+    {
+        cout<<"even";
+    }
+    else
+    {
+        cout<<"odd";
+    }
+    return 0;
+}
