c4b76766b06c0994dc7a6e2a54e558ca9e79bdb5
rd97 <31499424+rd97@users.noreply.github.com>
2018-02-12 15:48:48 +0530

    Create 68.cpp

---
+++ 68.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"number";
+    cin>>n;
+    if(n%7==0)
+    {
+        cout<<"yes";
+    }
+        else
+        {
+        cout<<"no";
+    }
+}
