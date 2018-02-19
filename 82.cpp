534e280ac88cef54b4fa81666d5ac97fc3bdd216
rd97 <31499424+rd97@users.noreply.github.com>
2018-02-19 19:55:14 +0530

    Create 82.cpp

---
+++ 82.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   float len,bre,area;
+   cout<<"length:";
+   cin>>len;
+   cout<<"breadth:";
+   cin>>bre;
+   area=len*bre;
+   cout<<area;
+   return 0;
+}
