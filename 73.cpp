cf1632fa3e95e511499e19b831e48b1de8a36e78
rd97 <31499424+rd97@users.noreply.github.com>
2018-02-19 15:55:24 +0530

    Create 73.cpp

---
+++ 73.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"my number:";
+  cin>>a;
+  if((a>=40) && (a<=70))
+  {
+      cout<<"yes";
+  }else
+  {
+      cout<<"no";
+  }
+    return 0;
+}
