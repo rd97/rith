b2c06db1ef96bffe32d253c475776cb85e13da0c
rd97 <31499424+rd97@users.noreply.github.com>
2018-02-19 15:44:45 +0530

    Create 74.cpp

---
+++ 74.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a;
+    int t;
+    cout<<"number:";
+    cin>>a;
+    cout<<"output:";
+    if(a>0)
+    {
+        t=a+0.5;
+    }else
+    {
+        t=a-0.5;
+    }
+    cout<<t;
+    return 0;
+}
