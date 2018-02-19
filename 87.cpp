fcba33fce5145c2929a28efa5f50068b96bcce4b
rd97 <31499424+rd97@users.noreply.github.com>
2018-02-19 16:20:57 +0530

    Create 87.cpp

---
+++ 87.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,gcd,i;
+    cout<<"enter the dividable num:";
+    cin>>a>>b;
+    for(i=1;i<=a&&i<=b;i++)
+    {
+        if(a%i==0&&b%i==0)
+        {
+            gcd=i;
+        }
+    }
+    cout<<gcd;
+    return 0;
+}
