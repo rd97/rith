38f43dbe4528546fdaedeae82dbb60902c4db509
rd97 <31499424+rd97@users.noreply.github.com>
2018-03-02 21:58:58 +0530

    Create 110.cpp

---
+++ 110.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,sum=0;
+    cout<<"num:";
+    cin>>n;
+    for(i=0;i<=n;i++)
+    {
+        sum=sum+i;
+    }cout<<sum;
+    return 0;
+}
