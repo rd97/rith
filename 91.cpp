5c67442f5bbdba18286e29d1c68e3b0bf803b38d
rd97 <31499424+rd97@users.noreply.github.com>
2018-03-02 16:18:00 +0530

    Create 91.cpp

---
+++ 91.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,b,h,c,d;
+    cin>>l>>b>>h;
+    c=l*b*h;
+    d=l*b;
+    cout<<"volume of cuboid"<<c<<'\n';
+    cout<<"volume of surface area"<<d;
+}
