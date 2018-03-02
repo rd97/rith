b399f1da3e3a87c849e9dfb9d3bb68aba2240982
rd97 <31499424+rd97@users.noreply.github.com>
2018-03-02 15:45:40 +0530

    Create 114.cpp

---
+++ 114.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k,a[60],i,j=0;
+    cout<<"value";
+    cin>>n>>k;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+        if(a[k]%2!=0)
+        {
+            cout<<a[k];
+        }
+    
+    return 0;
+}
