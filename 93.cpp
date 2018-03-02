4720aa1cb18b70507b553d60efe8154504057195
rd97 <31499424+rd97@users.noreply.github.com>
2018-03-02 16:25:12 +0530

    Create 93.cpp

---
+++ 93.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int temp,celsius=0;
+    cout<<"temperature";
+    cin>>temp;
+    celsius=temp+273;
+    cout<<celsius;
+}
