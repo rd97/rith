e64dbe64f35ab1227989d87410cd9decd9ad7259
rd97 <31499424+rd97@users.noreply.github.com>
2018-03-11 20:33:05 +0530

    Create 95.cpp

---
+++ 95.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float p,t,r,z;
+    cout<<"amount";
+    cin>>p;
+    cout<<"time";
+    cin>>t;
+    cout<<"rate";
+    cin>>r;
+    z=(p*t*r)/100;
+    cout<<z;
+}
