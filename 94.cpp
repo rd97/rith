59eb07b2b5dc98128561a3b85c57e9d4bafd22b1
rd97 <31499424+rd97@users.noreply.github.com>
2018-03-11 20:28:13 +0530

    Create 94.cpp

---
+++ 94.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k,a[60],i;
+    cout<<"value";
+    cin>>n>>k;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+        cout<<a[i];
+        break;
+        }
+    }return 0;
+}
