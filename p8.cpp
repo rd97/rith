5ac1365b87083debe1161082f523d18a603d4343
rd97 <31499424+rd97@users.noreply.github.com>
2018-03-14 16:34:11 +0530

    Update p8.cpp

--- p8.cpp
+++ p8.cpp
@@ -3,18 +3,18 @@ using namespace std;
 #include<string.h>
 int main() 
 {
-	char a[100];
-    int n,i;
-    gets(a);
-    a[0]=a[0]-32;
+	char t[10];  int i;
+    gets(c);
+    c[0]=c[0]-32;
     for(i=0;i<10;i++)
     {
-      if(a[i]==' ')
+      if(c[i]==' ')
       {
-          a[i+1]=a[i+1]-32;
+          c[i+1]=c[i+1]-32;
       }
     }
-    cout<<a;
+    cout<<c;
 	
 	return 0;
 }
+  
